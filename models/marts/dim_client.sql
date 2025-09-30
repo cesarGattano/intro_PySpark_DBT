eafbb7d15ce2b7ffd3a24a3d5ba926e38215898a
Cesar Gattano <cesar.gattano@gmail.com>
2025-09-30 15:31:08 +0200

    feat: :sparkles: Modèle dim_client

---
+++ models/marts/dim_client.sql
@@ -0,0 +1,15 @@
+WITH ventes AS (
+    SELECT * FROM {{ ref('stagged_ventes') }}
+),
+final AS (
+    SELECT
+        row_number() over () AS id,
+        client_nom AS nom,
+        client_age AS age,
+        client_ville AS ville
+    FROM ventes
+    GROUP BY client_nom, client_age, client_ville
+    ORDER BY client_nom, client_age, client_ville
+)
+
+SELECT * FROM final
