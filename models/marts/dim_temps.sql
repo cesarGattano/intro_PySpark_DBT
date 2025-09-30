06ba02b117aa24709f0882167d8b206acdd3c36b
Cesar Gattano <cesar.gattano@gmail.com>
2025-09-30 17:07:24 +0200

    feat: :sparkles: Model dim_temps

---
+++ models/marts/dim_temps.sql
@@ -0,0 +1,18 @@
+WITH ventes AS (
+    SELECT * FROM {{ ref('stagged_ventes') }}
+),
+final AS (
+    SELECT
+        date,
+        EXTRACT(YEAR FROM date) AS year,
+        EXTRACT(MONTH FROM date) AS month,
+        EXTRACT(WEEK FROM date) AS week,
+        EXTRACT(DAY FROM date) AS day,
+        EXTRACT(DOW FROM date) AS dow,
+        EXTRACT(DOY FROM date) AS doy
+    FROM ventes
+    GROUP BY date
+    ORDER BY date
+)
+
+SELECT * FROM final
